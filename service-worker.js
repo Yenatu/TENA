622137a524e5bd1d303927df614fcf588ff1f59d
monica84smith-svg <monica84smith@gmail.com>
2026-09-08 16:03:22 +0300

    Prepare Tena cafe app for deployment

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'tena-cafe-v1';
+const APP_SHELL = ['./tena.html', './manifest.json', './icon.svg'];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)));
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+    if (event.request.method !== 'GET') return;
+
+    event.respondWith(
+        caches.match(event.request).then((cachedResponse) => {
+            if (cachedResponse) return cachedResponse;
+            return fetch(event.request).then((networkResponse) => {
+                const responseCopy = networkResponse.clone();
+                caches.open(CACHE_NAME).then((cache) => cache.put(event.request, responseCopy));
+                return networkResponse;
+            });
+        })
+    );
+});
