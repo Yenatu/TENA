9cfa1bf57e56428c54ab60e1a2032837ab440c58
Ayene Addisie <ayeneaddisie19@gmail.com>
2026-09-08 16:30:35 +0100

    change nnn

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'tena-cafe-v3';
+const CACHE_NAME = 'tena-cafe-v4';
 const APP_SHELL = ['./', './index.html', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', (event) => {
@@ -7,11 +7,24 @@ self.addEventListener('install', (event) => {
 });
 
 self.addEventListener('activate', (event) => {
-    event.waitUntil(self.clients.claim());
+    event.waitUntil(
+        caches.keys().then((cacheNames) => Promise.all(
+            cacheNames
+                .filter((cacheName) => cacheName !== CACHE_NAME)
+                .map((cacheName) => caches.delete(cacheName))
+        )).then(() => self.clients.claim())
+    );
 });
 
 self.addEventListener('fetch', (event) => {
     if (event.request.method !== 'GET') return;
+
+    if (event.request.mode === 'navigate') {
+        event.respondWith(
+            fetch(event.request).catch(() => caches.match('./index.html'))
+        );
+        return;
+    }
 
     event.respondWith(
         caches.match(event.request).then((cachedResponse) => {
